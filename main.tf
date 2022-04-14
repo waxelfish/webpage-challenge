a9077d2810fad831b7517c535ef29c6cae908148
Axel Wagner <axwagner@yahoo.com>
2022-04-14 12:02:23 -0500

    Package install via ansible rather than cloud-init.

--- main.tf
+++ main.tf
@@ -12,9 +12,12 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   # GCP specific SSH key on VM is stored in console and will be added to VM automatically
-  metadata = {
-    user-data = "${data.cloudinit_config.conf.rendered}"
-  }
+
+  # diabled in favor of ansible playbook. Cloud-init runs in the background after VM initialization
+  # often interferring with package configuration if done via cloud-init rather than ansible!
+  #metadata = {
+  #  user-data = "${data.cloudinit_config.conf.rendered}"
+  #}
 
   network_interface {
     network = google_compute_network.vpc_network.name
@@ -29,7 +32,7 @@ resource "google_compute_instance" "vm_instance" {
     echo "webserver ansible_host=${google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip}" > inv
     echo -e "\n[all:vars]\nPKI_DIR=\"./pki\"\n" >> inv
     ansible-playbook -i inv local_playbook.yml --tags pki -e "gcp_ip_address=${google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip}"
-    ansible-playbook -i inv playbook.yml
+    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inv playbook.yml
     EOT
   }
 }
