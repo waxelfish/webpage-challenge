9e137eceb74aa5ee9e2f80a3d25c0547f0c4d47b
Axel Wagner <axwagner@yahoo.com>
2022-04-18 08:49:00 -0500

    Fully utilizing inventory

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "google_compute_instance" "vm_instance" {
     command     = <<EOT
     echo "webserver ansible_host=${google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip}" > inv
     echo -e "\n[all:vars]\nPKI_DIR=\"./pki\"\n" >> inv
-    ansible-playbook -i inv local_playbook.yml --tags "pki,ssh" -e "gcp_ip_address=${google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip}"
+    ansible-playbook -i inv local_playbook.yml --tags "pki,ssh"
     ansible-playbook -i inv playbook.yml
     EOT
   }
